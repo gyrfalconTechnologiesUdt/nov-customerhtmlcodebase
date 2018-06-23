655f4aaaf8d511a5672d18c992973f53f0f6885b
Arun <rani.arun@gmail.com>
2018-06-23 13:54:10 +0530

    updated Jquery Base code

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+//User Name Filed Key up Event
+$("#userName input").keyup(function(e){
+	console.log(e.currentTarget.value);
+});
+
+
+
+$("#testFunction").click(function(){
+	alert("success");
+});
+
+
+
+
+});
