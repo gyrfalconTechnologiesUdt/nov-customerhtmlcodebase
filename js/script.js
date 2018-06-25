8bdb830d9e5fe7324954568a322482bd4c24040b
Gayathri <gayathriloganathan18@gmail.com>
2018-06-25 10:00:55 +0530

    script updated

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
 $(document).ready(function(){
 
 //User Name Filed Key up Event
-$("#userName input").focus(function(e){
-	console.log(e.currentTarget.value);
+$("#userNameInpt input").focus(function(e){
+	//alert("hi");
+$("#usrNmLbl").addClass("usrNmLbl");	
+	
 });
-
-
+$("#userNameInpt input").blur(function(e){
+	//alert("hi");
+$("#usrNmLbl").removeClass("usrNmLbl");	
+	
+});
+$("#passWrdInpt input").focus(function(e){
+	//alert("hi");
+$("#paswrdLbl").addClass("paswrdLbl");	
+	
+});
+$("#passWrdInpt input").blur(function(e){
+	//alert("hi");
+$("#paswrdLbl").removeClass("paswrdLbl");	
+	
+});
 
 $("#testFunction").click(function(){
 	alert("success");
